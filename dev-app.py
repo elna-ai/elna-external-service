a1d43c19fe4c6d2a4a3c7402d80aad79dc5303ed
Tom Victor <vjtomvictor@icloud.com>
2023-12-22 16:01:17 +0100

    github pipeline testing

--- dev-app.py
+++ dev-app.py
@@ -18,13 +18,14 @@ ExternalServiceStack(app, f"{deployment_stage}-AiStack",
                      # Uncomment the next line to specialize this stack for the AWS Account
                      # and Region that are implied by the current CLI configuration.
 
-                     # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+                     # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv(
+                     # 'CDK_DEFAULT_REGION')),
 
                      # Uncomment the next line if you know exactly what Account and Region you
                      # want to deploy the stack to. */
 
                      env=cdk.Environment(account='931987803788', region='eu-north-1'),
-                     stage_name=deployment_stage
+                     stage_name="github"
 
                      # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                      )
