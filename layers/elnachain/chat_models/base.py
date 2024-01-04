33292658d333d95013817ff2522ba3f383c7c9ea
Alex Jolly <alexjolly28@gmail.com>
2024-01-04 22:15:35 +0530

    base chat generalised

--- layers/elnachain/chat_models/base.py
+++ layers/elnachain/chat_models/base.py
@@ -2,6 +2,7 @@
 
 Contains all the AI model related objects, parsing logic and implementations.
 """
+from elnachain.chat_models.messages import format_message
 
 
 class BaseModel:
@@ -11,39 +12,31 @@ class BaseModel:
 
     def __init__(self, client, logger):
         self._logger = logger
-        self._client = client
-        self._event = None
+        self._client=client
+        self._messages = None
         self._text_response = ""
         self._error_response = ""
 
     def __str__(self):
         return f"{self.model_name} - Model"
 
-    def get_request_messages(self) -> list:
-        """Get the request msg"""
-        messages = [
-            {"role": "system", "content": self.get_biography()},
-            {"role": "user", "content": self.get_input_prompt()},
-        ]
-        return messages
 
-    def create_response(self, event) -> bool:
+    def __call__(self, messages) -> bool:
         """Create the response message"""
-        self._event = event
         try:
-            response = self.client.chat.completions.create(
-                model=self.model_name, messages=self.get_request_messages()
+            response = self._client.chat.completions.create(
+                model=self.model_name, messages=format_message(messages)
             )
             self._text_response = self.parse_response(response)
         except Exception as e:
             self._error_response = e
-            return False
-        return True
+            return None
+        return self._text_response
 
     def parse_response(self, response):
         """Parse the response"""
-
-        self._logger.info(msg=f"ai raw response:{str(response)})")
+        if self._logger:
+            self._logger.info(msg=f"ai raw response:{str(response)})")
         result = response.choices[0].message.content.strip()
         return result
 
@@ -55,19 +48,4 @@ class BaseModel:
         """Get the error response"""
         return self._error_response
 
-    def get_biography(self) -> str:
-        """Get the biography"""
-        try:
-            bio = self._event["biography"]
-        except Exception as e:
-            raise Exception(f"unable to parse biography {str(e)}")
-
-        return bio
-
-    def get_input_prompt(self) -> str:
-        """Get the input prompt"""
-        try:
-            prompt = self._event["input_prompt"]
-        except Exception as e:
-            raise Exception(f"unable to parse input_prompt {str(e)}")
-        return prompt
+ 
