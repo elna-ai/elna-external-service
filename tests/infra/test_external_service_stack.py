ed6c262fd3730baef5fc50b7db5b309f65eb076f
Tom Victor <vjtomvictor@icloud.com>
2024-01-16 13:17:10 +0100

    add code format to ci

--- tests/infra/test_external_service_stack.py
+++ tests/infra/test_external_service_stack.py
@@ -3,10 +3,12 @@ import aws_cdk.assertions as assertions
 
 from infra.external_service_stack import ExternalServiceStack
 
+
 def test_sqs_queue_created():
     app = core.App()
     stack = ExternalServiceStack(app, "src")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
