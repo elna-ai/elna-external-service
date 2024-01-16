5083b767e4ba3803ee2b640605d344c6ba64f5f2
Tom Victor <vjtomvictor@icloud.com>
2024-01-16 13:26:42 +0100

    Update test_ai_models.py

--- tests/inference_engine/test_ai_models.py
+++ tests/inference_engine/test_ai_models.py
@@ -10,14 +10,9 @@ def test_mock_model():
     mock_biography = "bio_value"
     mock_input_prompt = "prompt_value"
 
-    mock_event = {"biography": mock_biography
-        ,
-                  "input_prompt":
-        mock_input_prompt}
-    selected_model_cls = choose_service_model(
-        mock_event, None)
-    ai_model = selected_model_cls(mock_event,
-                                  "asd_test")
+    mock_event = {"biography": mock_biography, "input_prompt": mock_input_prompt}
+    selected_model_cls = choose_service_model(mock_event, None)
+    ai_model = selected_model_cls(mock_event, "asd_test")
     assert ai_model.create_response() is True
     assert ai_model.get_text_response()[0] == mock_biography
     assert ai_model.get_text_response()[1] == mock_input_prompt
